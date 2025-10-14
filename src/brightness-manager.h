bf5b8ae0022a121372360a88b126d383a2d09731
Bardia Moshiri <bardia@furilabs.com>
2025-10-14 13:01:51 -0400

    Revert "brightness-manager: Add Brightness DBus interface"
    This reverts commit 57ee80353d5f7877d33ffd0e85be9dce66d3e7cd.

--- src/brightness-manager.h
+++ src/brightness-manager.h
@@ -6,16 +6,13 @@
 
 #pragma once
 
-#include "phosh-brightness-dbus.h"
-
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
 #define PHOSH_TYPE_BRIGHTNESS_MANAGER (phosh_brightness_manager_get_type ())
 
-G_DECLARE_FINAL_TYPE (PhoshBrightnessManager, phosh_brightness_manager, PHOSH, BRIGHTNESS_MANAGER,
-                      PhoshDBusBrightnessSkeleton)
+G_DECLARE_FINAL_TYPE (PhoshBrightnessManager, phosh_brightness_manager, PHOSH, BRIGHTNESS_MANAGER, GObject)
 
 PhoshBrightnessManager *phosh_brightness_manager_new (void);
 GtkAdjustment *         phosh_brightness_manager_get_adjustment (PhoshBrightnessManager *self);
